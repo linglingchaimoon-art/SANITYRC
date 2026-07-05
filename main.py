d4d38179a9b8462fca8331ddaf6226c2c7fbb7f7
linglingchaimoon-art <linglingchaimoon@gmail.com>
2026-07-05 03:14:57 +0200

    added VERCEL URL

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "https://sanity-rc-panel.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
